1b956df8b8bd92c1643abaa6cd99ba8d2fe77aa5
hjkim0905 <eax9952@gmail.com>
2023-11-19 17:00:11 +0900

    231119 현중 : App.js 파일 주석 추가

--- Frontend/test-project-front/src/App.js
+++ Frontend/test-project-front/src/App.js
@@ -1,15 +1,15 @@
-import logo from './logo.svg';
+import logo from "./logo.svg";
 import styled from "styled-components";
-import './App.css';
+import "./App.css";
 
 // 기본 배경 (for temp)
 const Div = styled.div`
   /* 가로, 길이 */
-  width : 50vw;
-  height : 50vh;
+  width: 50vw;
+  height: 50vh;
 
   /* 배치 */
-  margin : 25vh auto;
+  margin: 25vh auto;
   justify-content: center;
   align-items: center;
   display: flex;
@@ -21,7 +21,10 @@ const Div = styled.div`
 
 function App() {
   return (
-    <Div><h1>LDJ 2조 Main page</h1></Div>
+    // 메인 페이지입니다.
+    <Div>
+      <h1>LDJ 2조 Main page</h1>
+    </Div>
   );
 }
 
