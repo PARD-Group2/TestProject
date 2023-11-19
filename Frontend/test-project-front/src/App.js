26f5f40d747a3a6e4b7f41bc70526456a84925a6
pangil5634 <oksk6681@naver.com>
2023-11-19 16:53:48 +0900

    231119 메인 페이지 수정

--- Frontend/test-project-front/src/App.js
+++ Frontend/test-project-front/src/App.js
@@ -1,24 +1,27 @@
 import logo from './logo.svg';
+import styled from "styled-components";
 import './App.css';
+
+// 기본 배경 (for temp)
+const Div = styled.div`
+  /* 가로, 길이 */
+  width : 50vw;
+  height : 50vh;
+
+  /* 배치 */
+  margin : 25vh auto;
+  justify-content: center;
+  align-items: center;
+  display: flex;
+
+  /* 배경색 */
+  background-color: aqua;
+  border-radius: 30px;
+`;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Div><h1>LDJ 2조 Main page</h1></Div>
   );
 }
 
